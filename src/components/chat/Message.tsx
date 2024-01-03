13e9724e8a6347a9db3755b8d3b9e0798692eb02
p-nanaware <pnanaware.umassd.edu>
2024-01-02 23:52:46 -0500

    Added Message Display Name

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,14 +1,15 @@
 import { cn } from "@/lib/utils";
 import { ExtendedMessage } from "@/types/message";
-import { Icons } from "../ui/icons";
 import ReactMarkdown from "react-markdown";
 import { format } from "date-fns";
 import { forwardRef } from "react";
+import { Icons } from "../ui/icons";
 
 interface MessageProps {
   message: ExtendedMessage;
   isNextMessageSamePerson: boolean;
 }
+
 const Message = forwardRef<HTMLDivElement, MessageProps>(
   ({ message, isNextMessageSamePerson }, ref) => {
     return (
@@ -22,7 +23,7 @@ const Message = forwardRef<HTMLDivElement, MessageProps>(
           className={cn(
             "relative flex h-6 w-6 aspect-square items-center justify-center",
             {
-              "order-2 bg-rose-600 rounded-sm": message.isUserMessage,
+              "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
               "order-1 bg-zinc-800 rounded-sm": !message.isUserMessage,
               invisible: isNextMessageSamePerson,
             }
@@ -32,8 +33,9 @@ const Message = forwardRef<HTMLDivElement, MessageProps>(
             <Icons.user className="fill-zinc-200 text-zinc-200 h-3/4 w-3/4" />
           ) : (
             <Icons.logo className="fill-zinc-300 h-3/4 w-3/4" />
-          )}{" "}
+          )}
         </div>
+
         <div
           className={cn("flex flex-col space-y-2 text-base max-w-md mx-2", {
             "order-1 items-end": message.isUserMessage,
@@ -42,7 +44,7 @@ const Message = forwardRef<HTMLDivElement, MessageProps>(
         >
           <div
             className={cn("px-4 py-2 rounded-lg inline-block", {
-              "bg-rose-600 text-white": message.isUserMessage,
+              "bg-blue-600 text-white": message.isUserMessage,
               "bg-gray-200 text-gray-900": !message.isUserMessage,
               "rounded-br-none":
                 !isNextMessageSamePerson && message.isUserMessage,
@@ -77,5 +79,7 @@ const Message = forwardRef<HTMLDivElement, MessageProps>(
     );
   }
 );
+
+Message.displayName = "Message";
 
 export default Message;
